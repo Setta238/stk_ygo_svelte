2522a3ed50657a789a259147d329cbd21d375900
setta238 <238stk@gmail.com>
2025-03-31 19:08:51 +0900

    2025-03-31 フォルダを整理

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ export default {
     alias: {
       "@stk_utils": "./src/stk_utils",
       "@ygo":  "./src/ygo",
-      "@ygo_app":  "./src/ygo_app",
+      "@app":  "./src/app",
       "@ygo_deck_editor":  "./src/ygo_deck_editor",
       "@ygo_duel":  "./src/ygo_duel",
       "@ygo_duel_view": "src/ygo_duel_view",

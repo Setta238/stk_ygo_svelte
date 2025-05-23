7b432a5e7ca7e34fdaad9a2aa92bd428034d0617
setta238 <238stk@gmail.com>
2025-05-23 09:12:16 +0900

    苦渋の選択を実装

--- src/ygo_entity_proc/card_proc_definitions/CardProcDefinitions_NormalSpell.ts
+++ src/ygo_entity_proc/card_proc_definitions/CardProcDefinitions_NormalSpell.ts
@@ -31,9 +31,7 @@ export default function* generate(): Generator<EntityProcDefinition> {
         priorityForNPC: 40,
         fixedTags: ["SendToGraveyardFromDeck"],
         canExecute: (myInfo) => myInfo.activator.getDeckCell().cardEntities.some((card) => card.kind === "Monster"),
-        prepare: async () => {
-          return { selectedEntities: [] };
-        },
+        prepare: defaultPrepare,
         execute: async (myInfo) => {
           const monsters = myInfo.activator.getDeckCell().cardEntities.filter((entity) => entity.kind === "Monster");
           if (monsters.length === 0) {
@@ -67,9 +65,7 @@ export default function* generate(): Generator<EntityProcDefinition> {
         priorityForNPC: 40,
         fixedTags: ["SendToGraveyardFromDeck"],
         canExecute: (myInfo) => myInfo.activator.getDeckCell().cardEntities.some((card) => card.kind !== "Monster"),
-        prepare: async () => {
-          return { selectedEntities: [] };
-        },
+        prepare: defaultPrepare,
         execute: async (myInfo) => {
           const monsters = myInfo.activator.getDeckCell().cardEntities.filter((entity) => entity.kind !== "Monster");
           if (monsters.length === 0) {
@@ -80,6 +76,57 @@ export default function* generate(): Generator<EntityProcDefinition> {
             throw new IllegalCancelError(myInfo);
           }
           await target.sendToGraveyard(["Effect"], myInfo.action.entity, myInfo.activator);
+          myInfo.activator.getDeckCell().shuffle();
+
+          return true;
+        },
+        settle: async () => true,
+      },
+      defaultSpellTrapSetAction,
+    ],
+  };
+  yield {
+    name: "苦渋の選択",
+    actions: [
+      {
+        title: "発動",
+        isMandatory: false,
+        playType: "CardActivation",
+        spellSpeed: "Normal",
+        executableCells: ["Hand", "SpellAndTrapZone"],
+        executablePeriods: ["main1", "main2"],
+        executableDuelistTypes: ["Controller"],
+        priorityForNPC: 40,
+        fixedTags: ["SendToGraveyardFromDeck", "SearchFromDeck"],
+        canExecute: (myInfo) => myInfo.activator.getDeckCell().cardEntities.length > 4,
+        prepare: defaultPrepare,
+        execute: async (myInfo) => {
+          const cards = myInfo.activator.getDeckCell().cardEntities;
+          if (cards.length < 5) {
+            return false;
+          }
+          const list = await myInfo.activator.waitSelectEntities(cards, 5, (selected) => selected.length === 5, "相手に見せるカードを選択。", false);
+          if (!list) {
+            throw new IllegalCancelError(myInfo);
+          }
+
+          const selected = await myInfo.activator.getOpponentPlayer().waitSelectEntity(list, "手札に加えさせるカードを選択。", false);
+          if (!selected) {
+            throw new IllegalCancelError(myInfo, list);
+          }
+
+          myInfo.activator.writeInfoLog(`${myInfo.activator.getOpponentPlayer().name}は${selected.toString()}を選択。`);
+
+          //選択したカードを手札に加える
+          await selected.addToHand(["Effect"], myInfo.action.entity, myInfo.activator);
+
+          await DuelEntityShortHands.sendManyToGraveyardForTheSameReason(
+            list.filter((card) => card !== selected),
+            ["Effect"],
+            myInfo.action.entity,
+            myInfo.activator
+          );
+
           myInfo.activator.getDeckCell().shuffle();
 
           return true;
